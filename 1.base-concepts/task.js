bdd4abf4248ece0ae15f120ba2ae8544737de5e6
Natalia Alferova <natalia@alferova.me>
2022-03-28 11:57:10 +0200

    Version 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,37 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let result = [];
+  let D = Math.pow(b, 2) - 4 * a * c;
+
+  if (D > 0) {
+    result.push ((-b + Math.sqrt(D)) / (2 * a));
+    result.push ((-b - Math.sqrt(D)) / (2 * a));
+    return result;
+  }
+
+  if (D == 0) {
+    result.push (-b / (2 * a));
+    return result;
+  }
+
+  return result;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+
+
+
+
+
+
+
+
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//   let totalAmount;
+
+//   // код для задачи №2 писать здесь
+
+//   return totalAmount;
+// }

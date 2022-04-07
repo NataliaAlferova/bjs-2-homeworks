c3cd7cd82f50b0399bb3b97bd5671f84cc9e2252
Natalia Alferova <natalia@alferova.me>
2022-04-07 13:55:51 +0200

    Added worker

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,85 @@
+"use strict";
+
 // Задание 1
-function getArrayParams(arr) {
-  let min, max, sum, avg;
 
-  // Ваш код
+let numbers = [];
 
-  return { min: min, max: max, avg: avg };
+function getArrayParams(numbers){ 
+  let result = {
+    min: Infinity,
+    max: -Infinity,
+    avg: 0,
+  }
+  let sum = 0;
+
+  for (let number of numbers) {
+    if (number > result.max) {
+      result.max = number;
+    }
+  }
+
+  for (let number of numbers) {
+    if (number < result.min) {
+      result.min = number;
+    }
+  }
+
+  for (let number of numbers) {
+    sum = sum + number;
+    }
+  result.avg = sum / numbers.length;
+  result.avg = Number((result.avg).toFixed(2));
+
+  return result;
 }
 
 // Задание 2
-function worker(arr) {
-  let sum;
 
-  // Ваш код
+let arrOfArr = [];
 
+function worker(array) {
+  let sum = 0;
+  for (let number of array) {
+    sum = sum + number;
+    }
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
-
-  // Ваш кода
-  // for ...
-  
+function makeWork(arrOfArr, worker) {
+  let max = -Infinity;
+  for (let array of arrOfArr) {
+    if (worker(array) > max) {
+      max = worker(array);
+    }
+  }
   return max;
 }
 
+
 // Задание 3
-function worker2(arr) {
-  // Ваш код
+
+function worker2(array) {
+  let min = Infinity;
+  let max = -Infinity;
+  for (let number of array) {
+    if (number > max) {
+      max = number;
+    }
+    if (number < min) {
+      min = number
+    }
+  }
+  let diff = Math.abs(max - min);
+
+  return diff;
+}
+
+function makeWork(arrOfArr, worker2) {
+  let max = -Infinity;
+  for (let array of arrOfArr) {
+    if (worker2(array) > max) {
+      max = worker2(array);
+    }
+  }
+  return max;
 }

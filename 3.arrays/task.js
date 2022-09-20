94a345b0f8466c0a54e0f5e25664fb6492dd5c3e
Natalia Alferova <natalia@alferova.me>
2022-09-20 16:14:39 +0200

    Arrays V.3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,16 +1,10 @@
 function compareArrays(arr1, arr2) {
-  if (arr1.length === arr2.length){
-  result = arr1.every((element, index) => element === arr2[index]);
-  } else {
-    return false;
-  }
-  
-  return result; 
+  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+
 }
 
 
 function advancedFilter(arr) {
-  let resultArr = arr.filter(element => element > 0 && element % 3 === 0).map(element => element * 10);
+  return arr.filter(element => element > 0 && element % 3 === 0).map(element => element * 10);
 
-  return resultArr;
 }
